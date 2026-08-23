c6367897d11f575c7f92e3ae1c3ef508ae4531cc
Alvin Svensson <as228id@student.lnu.se>
2026-08-23 18:16:56 +0200

    question 1

---
+++ recitations/recitation_4/TurtleAGI.py
@@ -0,0 +1,138 @@
+import re
+
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+def turtleAGI(objective):
+
+    t_id = 1
+    initial_task = {
+        "task_id": t_id,
+        "task_name": "Develop a task list"
+    }
+
+    agenda = [initial_task]
+
+    while True:
+        if not agenda:
+            break
+
+        task = agenda.pop(0)
+
+        print(f"NEXT TASK: {task['task_name']}\n")
+
+        raw_solution = generate_raw_solution(objective, task["task_name"])
+        task_names = list(map(lambda x:x['task_name'],agenda))
+
+        new_tasks = normalize_solution(
+            objective,
+            raw_solution,
+            task["task_name"],
+            task_names
+        )
+
+        for new_task in new_tasks:
+            t_id +=1
+            new_task['task_id'] = t_id
+            agenda.append(new_task)
+
+        task_names = list(map(lambda x: x['task_name'], agenda))
+
+        prioritized_tasks = prioritize_solution(objective, task_names)
+
+        if prioritized_tasks:
+            agenda = prioritized_tasks
+
+def generate_raw_solution(objective, task):
+    prompt = f'Perform the following task based on the following objective: {objective}.\n'
+    prompt += f'\n task: {task}\nResponse:'
+    return llm_call(prompt, max_len=2000)
+
+def normalize_solution(objective, result, task_description, task_list):
+    prompt = f"""
+You are to use the raw task results to create new tasks with the following objective: {objective}.
+The last completed task has the raw result: \n{result}
+This result was based on this task description: {task_description}.\n"""
+
+    if task_list:
+        prompt += f"These are incomplete tasks: {', '.join(task_list)}\n"
+    prompt += "Based on the result, return a list of tasks to be completed in order to meet the objective. "
+    if task_list:
+        prompt += "These new tasks must not overlap with incomplete tasks. "
+
+    prompt += """
+Return one task per line in your response. The result must be a numbered list in the format:
+
+#. First task
+#. Second task
+
+The number of each entry must be followed by a period. If your list is empty, write "There are no tasks to add at this time."
+Unless your list is empty, do not include any headers before your numbered list or follow your numbered list with any other output."""
+
+    response = llm_call(prompt, max_len=2000)
+    new_tasks = response.split('\n')
+    new_tasks_list = []
+    for task_string in new_tasks:
+        task_parts = task_string.strip().split(".", 1)
+        if len(task_parts) == 2:
+            task_id = ''.join(s for s in task_parts[0] if s.isnumeric())
+            task_name = re.sub(r'[^\w\s_]+', '', task_parts[1]).strip()
+            if task_name.strip() and task_id.isnumeric():
+                new_tasks_list.append(task_name)
+
+    rv = [{"task_name": task_name} for task_name in new_tasks_list]
+    return rv
+
+def prioritize_solution(objective,task_names):
+
+    task_material = '\n' + '\n'.join(task_names)
+
+    prompt = f"""
+You are tasked with prioritizing the following tasks: {task_material}
+Consider the ultimate objective of your team: {objective}.
+Tasks should be sorted from highest to lowest priority, where higher-priority tasks are those that act as pre-requisites or are more essential for meeting the objective.
+Do not remove any tasks. Return the ranked tasks as a numbered list in the format:
+
+#. First task
+#. Second task
+
+The entries must be consecutively numbered, starting with 1. The number of each entry must be followed by a period.
+Do not include any headers before your ranked list or follow your list with any other output."""
+
+
+    response = llm_call(prompt, max_len=2000)
+    if not response:
+        print('Received empty response from priotritization agent. Keeping task list unchanged.')
+        return
+    new_tasks = response.split("\n") if "\n" in response else [response]
+    new_tasks_list = []
+    for task_string in new_tasks:
+        task_parts = task_string.strip().split(".", 1)
+        if len(task_parts) == 2:
+            task_id = ''.join(s for s in task_parts[0] if s.isnumeric())
+            task_name = re.sub(r'[^\w\s_]+', '', task_parts[1]).strip()
+            if task_name.strip():
+                new_tasks_list.append({"task_id": task_id, "task_name": task_name})
+
+    return new_tasks_list
+
+
+llm = ChatOllama(
+    model="gemma3",
+    temperature=0,
+)
+
+prompt_object = PromptTemplate(
+        template="{prompt}",
+        input_variables=["prompt"],
+    )
+
+chain = prompt_object | llm | StrOutputParser()
+
+def llm_call(prompt, max_len=2000):
+    response = chain.invoke({"prompt": prompt})
+    return response[:max_len]
+
+if __name__ == "__main__":
+    turtleAGI(input("what is the objective? (e.g. solve world hunger)"))
